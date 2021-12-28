f81475f3bd391618719d2f58acfb0ab2efc03d49
Ajay kattoju <96303899+ajay-aj7@users.noreply.github.com>
2021-12-28 21:31:35 +0530

    Add files via upload

---
+++ Divisible.java
@@ -0,0 +1,20 @@
+class Divisible 
+
+{
+public static void main(String[] args)
+{
+ int ch=12;
+ if (ch%3==0&&ch%4==0){
+System.out.println(ch+"good morning");
+}
+else if (ch%3==0){
+System.out.println(ch+ "good afternoon");
+}
+else if (ch%4==0){
+System.out.println("goodnight");
+}
+}
+}
+
+
+
