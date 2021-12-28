1bd6e04da70181edca6834f5e309c0ed3130a424
Ajay kattoju <96303899+ajay-aj7@users.noreply.github.com>
2021-12-28 21:36:11 +0530

    Add files via upload

---
+++ Largest.java
@@ -0,0 +1,36 @@
+class Largest {
+
+public static void main(String [] args){
+
+ int a = 1;
+
+ int b = 5;
+
+ int c = 2;
+  
+ String s =(a>b)?(a>c)? "a is big" : "c is big" :(c<b)? "b is     big" : "c is big";
+
+ System.out.println(s);
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+}
+
+
+
+
+
+
+
+
