c0d83a3fffbcc644a8b72d5fcf38cf12a18c3640
Ajay kattoju <96303899+ajay-aj7@users.noreply.github.com>
2021-12-28 21:32:22 +0530

    Add files via upload

---
+++ fact.java
@@ -0,0 +1,20 @@
+class Fact {
+public static void main(String[] args) {
+System.out.println(factorial());
+}
+public static int factorial(){
+int fact=1;
+for(int i=1;i<=5;i++){
+fact=fact*i;
+}
+return fact;
+}
+}
+
+
+
+
+
+
+
+
