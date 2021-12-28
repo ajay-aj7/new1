6014fff56d45e627e8e16cf4ed616d086d3f3bda
Ajay kattoju <96303899+ajay-aj7@users.noreply.github.com>
2021-12-28 21:32:13 +0530

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,30 @@
+  class EvenOdd {
+
+
+  public static void main (String[] args){
+     
+
+    int a=3;
+    
+    
+   
+   String s =(a%2==0) ? "a is a even number" : "a is a odd number";
+   
+   System.out.println(s);
+   
+}
+}
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
