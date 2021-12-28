cf8d99c51b597871710c41029c37ee0a29111589
Ajay kattoju <96303899+ajay-aj7@users.noreply.github.com>
2021-12-28 21:30:34 +0530

    Add files via upload

---
+++ CheckDivisible.java
@@ -0,0 +1,44 @@
+class CheckDivisible {
+    
+
+
+   public static void main (String[] args){
+ 
+   int a=12%3;
+  
+   int b=79%3;
+    
+   String s = b!=0 ? "good morning" : "good night";
+ 
+
+   System.out.println(s);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+}
+
+
+
+
+
+
+
+
