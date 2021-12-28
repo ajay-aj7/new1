238db26de7a1485fe4d26d6cdc323a8ce228ec5e
Ajay kattoju <96303899+ajay-aj7@users.noreply.github.com>
2021-12-28 21:35:56 +0530

    Add files via upload

---
+++ ForLoop93.java
@@ -0,0 +1,23 @@
+class ForLoop93
+{
+public static void main(String[] args)
+{
+int x=5;
+for(int i=1; i<=x;i++)
+{
+for(int j=1; j<=x; j++)
+{
+if((i==j)||(i+j==6))
+
+System.out.print("*");
+
+else
+System.out.print(" "); 
+}
+System.out.println( );
+}
+}
+}
+
+
+
