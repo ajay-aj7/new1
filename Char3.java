97c013d892fe11611e3d1186505272a13c8ebc62
Ajay kattoju <96303899+ajay-aj7@users.noreply.github.com>
2021-12-28 21:30:04 +0530

    Add files via upload

---
+++ Char3.java
@@ -0,0 +1,21 @@
+class Char3{
+
+public static void main(String[] args){
+ char ch = 'A';
+ int i =1;
+ 
+while(i<=26){
+System.out.println(ch+""+i+(char)(ch+32));
+ch++;
+i++;
+ 
+
+}
+}
+}
+
+
+
+
+///A1a
+
