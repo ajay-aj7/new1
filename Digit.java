13093ebc4f0dc1ec14865e123fec9a043e38ed16
Ajay kattoju <96303899+ajay-aj7@users.noreply.github.com>
2021-12-28 21:31:14 +0530

    Add files via upload

---
+++ Digit.java
@@ -0,0 +1,30 @@
+class Digit {
+ 
+  public static void main(String[] args){
+  
+ 
+char ch ='o';
+
+  if (ch>='o'&&ch<='a'){
+
+
+
+System.out.println(ch+  "contain a digit");
+}
+
+
+else{
+ System.out.println(ch+ "does not contain a digit");
+
+}
+
+}
+
+}
+
+
+
+
+
+
+
