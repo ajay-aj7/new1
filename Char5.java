afbffb63787d7d7c9fba143dc80a3c500877cbb9
Ajay kattoju <96303899+ajay-aj7@users.noreply.github.com>
2021-12-28 21:30:24 +0530

    Add files via upload

---
+++ Char5.java
@@ -0,0 +1,30 @@
+class Char5{
+
+public static void main(String[] args){
+  char ch ='*';
+int i =0;
+while(i<=26){
+ System.out.println(ch);
+ i++;
+}
+
+}
+}
+
+
+
+
+
+     
+
+
+
+
+    
+
+
+ 
+
+
+
+        
